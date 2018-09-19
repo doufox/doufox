54f4193683a847f878ba4fa5be6654c65412beb1
唐钰豆豆 <doudoudzj@sina.com>
2018-09-19 15:39:55 +0800

    登陆

--- core/admin/login.tpl.php
+++ core/admin/login.tpl.php
@@ -7,10 +7,9 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
 	<meta name="author" content="doufox.com" />
-	<meta name="copyright" content="Copyright (c) doufox ( A member of Crogram, Inc.) All Rights Reserved." />
+	<meta name="copyright" content="Copyright &copy; doufox ( A member of Crogram, Inc.) All Rights Reserved." />
 	<link href="/static/css/login.css" rel="stylesheet" type="text/css" />
 	<link rel="shortcut icon" href="/favicon.ico" mce_href="/favicon.ico" type="image/x-icon">
-	<script type="text/javascript" src="/static/js/jquery.min.js"></script>
 </head>
 
 <body>
@@ -20,23 +19,24 @@
 				<form class="cell" action="" method="post">
 					<div class="row">
 						<div class="cell l">帐号</div>
-						<div class="cell r"><input type="text" id="username" name="username" class="text" value="" maxlength="20" /></div>
+						<div class="cell r"><input type="text" name="username" class="text" maxlength="20" /></div>
 					</div>
 					<div class="row">
 						<div class="cell l">密码</div>
-						<div class="cell r"><input name="password" type="password" class="text" value="" maxlength="20" autocomplete="off" /></div>
+						<div class="cell r"><input name="password" type="password" class="text" maxlength="20" autocomplete="off" /></div>
 					</div>
 					<div class="row">
 						<div class="cell l">验证码</div>
 						<div class="cell r">
 							<input type="text" name="code" class="text captcha" maxlength="4" autocomplete="off" />
-							<img id="checkcode" src="<?php echo url("api/access/checkcode/ ", array("width " => 85, "height " => 26)); ?>" title="看不清楚？换一张">
+							<img id="checkcode" src="<?php echo url("api/access/checkcode", array("width" => 85, "height" => 26)); ?>" title="看不清楚？换一张">
 						</div>
 					</div>
 					<div class="row">
 						<div class="cell l"></div>
 						<div class="cell r">
-							<input type="submit" class="login-submit" value="登录" name="submit" />
+							<button name="submit" class="login-submit">登录</button>
+							<!-- <input type="submit" class="login-submit" value="登录" name="submit" /> -->
 						</div>
 					</div>
 				</form>
