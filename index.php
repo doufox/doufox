c7bba54f9e189384b4b98da3a191d7fd45e9ee0f
唐钰豆豆 <doudoudzj@sina.com>
2018-09-21 21:40:18 +0800

    入口文件调整

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ENTRY_FILE', 'index.php'); // 系统入口文件, 即当前文件
-define('CORE_DIR', 'core'); // 系统核心模块所在目录
-define('ROOT_PATH', dirname(__FILE__) . DS); // 网站文件跟目录, 即当前文件所在目录
+define('ENTRY_FILE', 'index.php'); // 系统入口文件
+define('CORE_DIR', 'core'); // 系统核心模块
+define('ROOT_PATH', dirname(__FILE__) . DS); // 系统根路径
 
 include ROOT_PATH . CORE_DIR . DS . 'app.php';
 cms::run();
