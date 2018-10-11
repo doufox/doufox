508ba75911d4920efa8fad71b12d16060b6aeb4d
唐钰豆豆 <doudoudzj@sina.com>
2018-10-11 21:35:40 +0800

    database backup

--- core/views/admin/database_import.tpl.php
+++ core/views/admin/database_import.tpl.php
@@ -1,10 +1,11 @@
-<?php include $this->admin_tpl('header');?>
+<?php include $this->admin_tpl('header'); ?>
+
 <script type="text/javascript">
-top.document.getElementById('position').innerHTML = '数据库恢复';
+	top.document.getElementById('position').innerHTML = '数据库恢复';
 </script>
 <div class="subnav">
 	<div class="content-menu">
-		<a href="<?php echo url('admin/database'); ?>"  class="on"><em>数据库备份</em></a>
+		<a href="<?php echo url('admin/database'); ?>" class="on"><em>数据库备份</em></a>
 	</div>
 	<div class="bk10"></div>
 
@@ -13,45 +14,61 @@ top.document.getElementById('position').innerHTML = '数据库恢复';
 <table width="100%" cellspacing="0">
     <thead>
        <tr>
-           <th width="33" align="right"><input name="selectform" class="cselectform" type="checkbox" onClick="setC()"></th>
-           <th width="150" align="left">备份时间</th>
-           <th >备份文件目录</th>
-           <th width="111">文件大小</th>
-           <th width="80" align="left" >操作</th>
+			<th align="left" width="50">
+				<label><input name="selectform" class="select_backups" type="checkbox" onClick="selectBackup()">&nbsp;全选</label>
+			</th>
+           <th align="left" width="150">备份时间</th>
+           <th align="left">备份文件目录</th>
+           <th align="left" width="150">文件大小</th>
+           <th align="left" width="150">操作</th>
        </tr>
     </thead>
     <tbody class="line-box">
-	<?php 
-	if ($list) {
-	foreach($list as $v){?>
-	<tr>
-	<td align="right"><input class="selectform" type="checkbox" name="paths[]" value="<?php echo $v['path']?>"/></td>
-	<td align="left"><?php echo date('Y-m-d H:i:s', $v['path'])?></td>
-	<td align="left"><?php echo $v['sqldir']?></td>
-	<td align="center"><?php echo $v['size']?></td>
-	<td  align="left">
-	<a href="javascript:admin_command.confirmurl('<?php echo url("admin/database/import", array("path"=>$v['path']))?>', '确定恢复数据库吗')">恢复数据库</a>
-    </td>
-	</tr>
-	<?php } }?>
-    <tr height="28">
-	<td colspan="5" align="left"><input type="submit" class="button" value="批量删除" name="submit" >&nbsp;备份目录：/data/bakup/
-</td>
-	</tr>
+		<?php if ($list) { foreach($list as $v){ ?>
+		<tr>
+			<td>
+				<label><input class="selectform" type="checkbox" name="paths[]" value="<?php echo $v['path']?>"/>&nbsp;选择<label>
+			</td>
+			<td><?php echo date('Y-m-d H:i:s', $v['path'])?></td>
+			<td><?php echo $v['sqldir']?></td>
+			<td><?php echo $v['size']?></td>
+			<td>
+				<a href="javascript:admin_command.confirmurl('<?php echo url("admin/database/import", array("path"=>$v['path']))?>', '确定恢复数据库吗')">恢复</a>
+				<a href="javascript:showTableBackups(<?php echo $v['path']?>);">预览</a>
+				<a href="javascript:void(0);">下载</a>
+				<a href="javascript:admin_command.confirmurl('<?php echo url("admin/database", array("path"=>$v['path']))?>', '确定删除吗？');">删除</a>
+			</td>
+		</tr>
+		<?php } }?>
+		<tr height="28">
+			<td colspan="5" align="left">
+				<input type="submit" class="button" value="批量删除" name="submit" >&nbsp;备份目录：/data/bakup/
+			</td>
+		</tr>
 	</tbody>
 </table>
 </form>
 </div>
-	
+
 </div>
-<script language="javascript">
-function setC() {
-	if($(".cselectform").attr('checked')) {
-		$(".selectform").attr("checked",true);
-	} else {
-		$(".selectform").attr("checked",false);
+<script type="text/javascript">
+	function selectBackup() {
+		if ($('.select_backups').prop('checked')) {
+			$('.selectform').prop('checked', true);
+		} else {
+			$('.selectform').prop('checked', false);
+		}
 	}
-}
+
+	function showTableBackups (path) {
+		window.top.art.dialog({
+			title: '查看备份数据' + path, 
+			id: 'show', 
+			iframe: '<?php echo url("admin/database/view")?>&file=' + path,
+			width: '700px',
+			height: '400px'
+		});
+	}
 </script>
 </body>
 </html>
--- core/views/admin/database_list.tpl.php
+++ core/views/admin/database_list.tpl.php
@@ -14,69 +14,69 @@ top.document.getElementById('position').innerHTML = '数据库备份';
 <table width="100%" cellspacing="0">
     <thead>
        <tr>
-           <th width="80" align="left">&nbsp;</th>
+           <th align="left" width="80">选择</th>
            <th align="left">表名</th>
            <th align="left">类型</th>
            <th align="left">编码</th>
-           <th align="left">记录数</th>
+           <th align="left">行数</th>
            <th align="left">使用空间</th>
            <th align="left">碎片</th>
-           <th width="150" align="left">操作</th>
+           <th align="left" width="150">操作</th>
        </tr>
     </thead>
     <tbody class="line-box">
-	<?php foreach($data as $v){?>
-	<tr>
-		<td align="left">
-			<input <?php if ($v['tables_sys']) { echo 'class="selectform"';}?> type="checkbox" name="table[]" value="<?php echo $v['Name'];?>"/>
-			<?php if ($v['tables_sys']) { echo '<font color="#f00">系统表</font>';} else {echo '<font color="#369">其他表</font>';}?>
-		</td>
-		<td align="left"><?php echo $v['Name']?></td>
-		<td align="left"><?php echo $v['Engine']?></td>
-		<td align="left"><?php echo $v['Collation']?></td>
-		<td align="left"><?php echo $v['Rows']?></td>
-		<td align="left"><?php echo formatFileSize($v['Data_length']+$v['Index_length'])?></td>
-		<td align="left"><?php echo formatFileSize($v['Data_free'])?></td>
-		<td align="left">
-			<a href="<?php echo url("admin/database/repair", array("name"=>$v['Name']))?>">修复</a> | 
-			<a href="<?php echo url("admin/database/optimize", array("name"=>$v['Name']))?>">优化</a> | 
-			<a href="javascript:void(0);" onclick="showcreat('<?php echo $v['Name']?>')">结构</a>
-		</td>
-	</tr>
-	<?php }
-	if (is_array($data)) {
-	?>
-    <tr height="28">
-		<td colspan="8" align="left">
-			<span>选择系统表&nbsp;&nbsp;</span>
-			<input name="selectform" class="cselectform" type="checkbox" onClick="setC()">&nbsp;&nbsp;
-			<input type="submit" class="button" value="开始备份数据库" name="submit">
-		</td>
-	</tr>
-    <?php }?>
+		<?php foreach($data as $v){ ?>
+		<tr>
+			<td>
+				<label>
+					<input <?php if ($v['tables_sys']) { echo 'class="selectform"';}?> type="checkbox" name="table[]" value="<?php echo $v['Name'];?>"/>
+					<?php if ($v['tables_sys']) { echo '<font color="#f00">系统表</font>';} else {echo '<font color="#369">其他表</font>';}?>
+				</label>
+			</td>
+			<td><?php echo $v['Name']?></td>
+			<td><?php echo $v['Engine']?></td>
+			<td><?php echo $v['Collation']?></td>
+			<td><?php echo $v['Rows']?></td>
+			<td><?php echo formatFileSize($v['Data_length'] + $v['Index_length'])?></td>
+			<td><?php echo formatFileSize($v['Data_free'])?></td>
+			<td>
+				<a href="<?php echo url("admin/database/repair", array("name"=>$v['Name']))?>">修复</a> | 
+				<a href="<?php echo url("admin/database/optimize", array("name"=>$v['Name']))?>">优化</a> | 
+				<a href="javascript:void(0);" onclick="showTableStructure('<?php echo $v['Name']?>')">结构</a>
+			</td>
+		</tr>
+		<?php } if (is_array($data)) { ?>
+		<tr height="28">
+			<td colspan="8">
+				<label><input name="selectform" class="select_tables" type="checkbox" onClick="selectTables()">&nbsp;选择系统表</label>&nbsp;&nbsp;
+				<input type="submit" class="button" value="开始备份数据库" name="submit">
+			</td>
+		</tr>
+		<?php } ?>
 	</tbody>
 </table>
 <div class="btn">&nbsp;</div>
 </form>
 </div>
 </div>
-<script language="javascript">
-function setC() {
-	if($(".cselectform").attr('checked')) {
-		$(".selectform").attr("checked",true);
-	} else {
-		$(".selectform").attr("checked",false);
+<script type="text/javascript">
+	function selectTables () {
+		if ($('.select_tables').prop('checked')) {
+			$('.selectform').prop("checked", true);
+		} else {
+			$('.selectform').prop("checked", false);
+		}
 	}
-}
-function showcreat(tblname) {
-	window.top.art.dialog({
-		title:tblname+'表结构', 
-		id:'show', 
-		iframe:'<?php echo url("admin/database/table", null, 1)?>&name='+tblname,
-		width:'700px',
-		height:'400px'
-	});
-}
+
+	function showTableStructure (table) {
+		window.top.art.dialog({
+			title: table + '表结构', 
+			id: 'show', 
+			iframe: '<?php echo url("admin/database/table")?>&name=' + table,
+			width: '700px',
+			height: '400px'
+		});
+	}
 </script>
 </body>
 </html>
