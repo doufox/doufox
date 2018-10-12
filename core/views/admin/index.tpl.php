309b9b2c476efc94fcb8b44135a06b43d6762ace
唐钰豆豆 <doudoudzj@sina.com>
2018-10-12 18:37:59 +0800

    update

--- core/views/admin/index.tpl.php
+++ core/views/admin/index.tpl.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="robots" content="none, nofollow, noarchive, nocache">
     <meta name="referrer" content="never"/>
     <meta name="generator" content="<?php echo APP_NAME; ?>"/>
     <title><?php echo $this->site_config['SITE_NAME']; ?> - 网站管理系统后台</title>
---
+++ doc/doc_template_tags.md
@@ -0,0 +1,21 @@
+# 模板标签的使用
+
+
+基本标签
+```
+{$site_title}       // 网页标题
+{$site_keywords}    // 网页关键字
+{$site_description} // 网页描述
+
+{$site_url}        // 网站的网址
+{$site_name}       // 网站的名称
+{$site_template}   // 网站的主题
+
+
+```
+
+加载模板标签
+```
+{template header.html} // 加载当前模板根目录下 header.html 模板文件
+{template member/index.html} // 加载当前模板目录下的 member 目录里的 index.html模板文件
+```
