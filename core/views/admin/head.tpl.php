5152e9e86d27bb2712e786c0949ea6bd20a2a87c
唐钰豆豆 <doudoudzj@sina.com>
2018-10-15 17:18:00 +0800

    update

--- core/views/admin/head.tpl.php
+++ core/views/admin/head.tpl.php
@@ -10,16 +10,16 @@
         <li id="_MP104">
             <a href="javascript:_MP(104, '<?php echo url('admin/index/config'); ?>');">设置</a>
             <ul>
-                <li id="_MP1041"><a href="javascript:_MP(1041,'<?php echo url('admin/index/config', array('type'=> 1)); ?>');">系统设置</a></li>
-                <li id="_MP1042"><a href="javascript:_MP(1042,'<?php echo url('admin/index/config', array('type'=> 2)); ?>');">水印设置</a></li>
-                <li id="_MP1043"><a href="javascript:_MP(1043,'<?php echo url('admin/index/config', array('type'=> 3)); ?>');">后台密码</a></li>
-                <li id="_MP1044"><a href="javascript:_MP(1044,'<?php echo url('admin/index/config', array('type'=> 4)); ?>');">会员配置</a></li>
-                <li id="_MP1045"><a href="javascript:_MP(1045,'<?php echo url('admin/index/config', array('type'=> 5)); ?>');">URL设置</a></li>
+                <li id="_MP1041"><a href="javascript:_MP(1041,'<?php echo url('admin/index/config', array('type'=>1)); ?>');">系统设置</a></li>
+                <li id="_MP1042"><a href="javascript:_MP(1042,'<?php echo url('admin/index/config', array('type'=>2)); ?>');">水印设置</a></li>
+                <li id="_MP1043"><a href="javascript:_MP(1043,'<?php echo url('admin/index/config', array('type'=>3)); ?>');">后台密码</a></li>
+                <li id="_MP1044"><a href="javascript:_MP(1044,'<?php echo url('admin/index/config', array('type'=>4)); ?>');">会员配置</a></li>
+                <li id="_MP1045"><a href="javascript:_MP(1045,'<?php echo url('admin/index/config', array('type'=>5)); ?>');">URL设置</a></li>
                 <li id="_MP107"><a href="javascript:_MP(107,'<?php echo url('admin/index/cache'); ?>');">更新缓存</a></li>
                 <li id="_MP403"><a href="javascript:_MP(403,'<?php echo url('admin/content/updateurl'); ?>');">更新内容URL</a></li>
                 <li id="_MP403"><a href="javascript:_MP(403,'<?php echo url('admin/database'); ?>');">数据库备份</a></li>
                 <li id="_MP1046"><a href="javascript:_MP(1046,'<?php echo url('admin/model'); ?>');">内容模型</a></li>
-                <li id="_MP1047"><a href="javascript:_MP(1047,'<?php echo url('admin/model', array('typeid'=> 3)); ?>');">表单模型</a></li>
+                <li id="_MP1047"><a href="javascript:_MP(1047,'<?php echo url('admin/model', array('typeid'=>3)); ?>');">表单模型</a></li>
                 <?php if (is_array($menu)) {foreach ($menu as $t) {?>
                 <li id="_MP9<?php echo $t['id'] ?>">
                     <a href="javascript:_MP(9<?php echo $t['id'] ?>,'<?php echo $t['url'] ?>');"><?php echo $t['name'] ?></a>
@@ -32,9 +32,9 @@
         <li id="_MP106">
             <a href="javascript:_MP(106,'<?php echo url('admin/createhtml'); ?>');">生成</a>
             <ul>
-                <li id="_MP1061"><a href="javascript:_MP(1061,'<?php echo url('admin/createhtml/index'); ?>&a=index');">生成首页</a></li>
-                <li id="_MP1062"><a href="javascript:_MP(1062,'<?php echo url('admin/createhtml'); ?>&a=category');">生成栏目页</a></li>
-                <li id="_MP1063"><a href="javascript:_MP(1063,'<?php echo url('admin/createhtml'); ?>&a=show');">生成内容页</a></li>
+                <li id="_MP1061"><a href="javascript:_MP(1061,'<?php echo url('admin/createhtml/index'); ?>');">生成首页</a></li>
+                <li id="_MP1062"><a href="javascript:_MP(1062,'<?php echo url('admin/createhtml/category'); ?>');">生成栏目页</a></li>
+                <li id="_MP1063"><a href="javascript:_MP(1063,'<?php echo url('admin/createhtml/show'); ?>');">生成内容页</a></li>
                 <li class="menubtm"></li>
             </ul>
         </li>
