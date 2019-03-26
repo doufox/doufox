531f76db5e31b5dca9bf54d62408b1eb2b85bfe8
doudoudzj <doudoudzj@gmail.com>
2019-03-27 00:51:51 +0800

    add a bootstrap theme

---
+++ data/theme/bootstrap/config.php
@@ -0,0 +1,53 @@
+<?php return array (
+  'file_explan' => 
+  array (
+    'data|theme|bootstrap|' => 
+    array (
+      'css' => '样式',
+      'img' => '图片',
+      'member' => '会员中心模板',
+      'banner.html' => '横幅图片模板',
+      'bookmark_category.html' => '书签栏目模板',
+      'bookmark_list.html' => '书签列表模板',
+      'bookmark_show.html' => '书签内容展示模板',
+      'category_article.html' => '文章栏目模板',
+      'category_download.html' => '下载栏目模板',
+      'category_product.html' => '产品栏目模板',
+      'config.php' => '模板备注文件',
+      'crumbs.html' => '导航栏',
+      'footer.html' => '通用底部模板',
+      'form.html' => '表单模板',
+      'header.html' => '通用头部模板',
+      'index.html' => '首页模板',
+      'list_article.html' => '文章列表模板',
+      'list_download.html' => '下载列表模板',
+      'list_product.html' => '产品列表模板',
+      'page.html' => '单页面模板',
+      'post.html' => '游客投稿模板',
+      'recommend_article.html' => '推荐文章',
+      'search.html' => '搜索模板',
+      'show_article.html' => '文章内容展示模板',
+      'show_download.html' => '下载内容展示模板',
+      'show_product.html' => '产品内容展示模板',
+      'sidebar.html' => '内页左侧模板',
+    ),
+    'data|theme|bootstrap|css|' => 
+    array (
+      'style.css' => '公共样式',
+    ),
+    'data|theme|bootstrap|member|' => 
+    array (
+      'content_add.html' => '添加内容',
+      'content_list.html' => '内容列表',
+      'form_list.html' => '',
+      'form_show.html' => '',
+      'index.html' => '用户首页模板',
+      'login.html' => '用户登陆模板',
+      'password.html' => '用户密码模板',
+      'profile_edit.html' => '用户资料修改模板',
+      'register.html' => '用户注册模板',
+      'sidebar.html' => '',
+      'user.html' => '',
+    ),
+  ),
+);?>
