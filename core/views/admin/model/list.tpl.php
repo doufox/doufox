4514cba51ce5a873aef653a39e5ab60740155ce6
doudoudzj <doudoudzj@gmail.com>
2019-09-09 17:27:53 +0800

    模型update

--- core/views/admin/model/list.tpl.php
+++ core/views/admin/model/list.tpl.php
@@ -1,18 +1,6 @@
 <?php include $this->admin_tpl('header'); ?>
 <?php include $this->admin_tpl('navbar'); ?>
 
-<script type="text/javascript">
-    function cdisabled(id, c) {
-        if (c == 1) {
-            window.location.href = "<?php echo url('admin/model/cdisabled/', array('typeid' => $typeid, 'modelid' => '')); ?>" + id;
-            return true;
-        }
-        if (confirm('禁用模型之后，该模型是数据将会无法访问，确定禁用吗？')) {
-            window.location.href = "<?php echo url('admin/model/cdisabled/', array('typeid' => $typeid, 'modelid' => '')); ?>" + id;
-        }
-    }
-</script>
-
 <div class="container">
     <div class="page_menu">
         <div class="panel panel-default">
@@ -47,18 +35,19 @@
                     <?php if (is_array($list)) {
                         foreach ($list as $t) {
                             $setting = string2array($t['setting']);
-                            $disable = isset($setting['disable']) && $setting['disable'] == 1 ? 1 : 0; ?>
+                            $disable = isset($setting['disable']) && $setting['disable'] == 1 ? 1 : 0;
+                            $status = $disable == 1 ? '<font color=red>启用</font>' : '禁用';
+                            unset($setting);
+                        ?>
                             <tr>
-                                <td align="left"><?php echo $t['modelid']; ?></td>
-                                <td align="left"><?php echo $t['modelname']; ?></td>
-                                <td align="left"><?php echo $modelTypeName[$t['typeid']]; ?></td>
-                                <td align="left"><?php echo $t['tablename']; ?></td>
-                                <td align="left">
+                                <td><?php echo $t['modelid']; ?></td>
+                                <td><?php echo $t['modelname']; ?></td>
+                                <td><?php echo $modelTypeName[$t['typeid']]; ?></td>
+                                <td><?php echo $t['tablename']; ?></td>
+                                <td>
                                     <a href="<?php echo url('admin/model/fields', array('typeid' => $typeid, 'modelid' => $t['modelid'])); ?>">字段管理</a>
                                     <a href="<?php echo url('admin/model/edit', array('typeid' => $typeid, 'modelid' => $t['modelid'])); ?>">编辑</a>
-                                    <a href="javascript:cdisabled(<?php echo $t['modelid']; ?>, <?php echo $disable; ?>);"><?php if ($disable) { ?><font color=red><?php echo '启用'; ?></font><?php } else {
-                                                                                                                                                                                                    echo '禁用 ';
-                                                                                                                                                                                                } ?></a>
+                                    <a href="javascript:model_disable('<?php echo $t['modelid']; ?>', '<?php echo $disable; ?>');"><?php echo $status; ?></a>
                                     <a href="javascript:model_delete('<?php echo $t['modelid']; ?>', '<?php echo $t['modelname']; ?>')">删除</a>
                                 </td>
                             </tr>
@@ -70,20 +59,18 @@
     </div>
 </div>
 
-<!-- 删除提示 -->
-<div class="modal fade" id="modal-model-delete" tabindex="-1" role="dialog" aria-labelledby="aria-model-delete">
+<!-- 提示 -->
+<div class="modal fade" id="modal-model-alert" tabindex="-1" role="dialog" aria-labelledby="aria-model-alert">
     <div class="modal-dialog modal-sm" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="关闭"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="aria-model-delete">系统提示</h4>
+                <h4 class="modal-title" id="aria-model-alert">系统提示</h4>
             </div>
-            <div class="modal-body">
-                <p>确定删除模型<span id="model-delete-name"></span>吗？</p>
-            </div>
+            <div class="modal-body" id="model-alert-body"></div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                <a type="button" id="model-delete-url" class="btn btn-primary" href="#">确定</a>
+                <a type="button" id="model-alert-url" class="btn btn-primary" href="#">确定</a>
             </div>
         </div>
     </div>
@@ -92,14 +79,19 @@
 <script type="text/javascript">
     function model_delete(id, name) {
         if (id && name) {
-            $('#modal-model-delete').modal();
-            document.getElementById('model-delete-url').href = '<?php echo url("admin/model/del", array("modelid" => "")); ?>' + id;
-            document.getElementById('model-delete-name').innerText = '"' + name + '"';
+            $('#modal-model-alert').modal();
+            document.getElementById('model-alert-url').href = '<?php echo url("admin/model/del", array("modelid" => "")); ?>' + id;
+            document.getElementById('model-alert-body').innerText = '确定删除模型"' + name + '"吗？';
         }
     }
-    $('#modal-model-delete').on('hide.bs.modal', function() {
-        document.getElementById('model-delete-url').href = '';
-        document.getElementById('model-delete-name').innerText = '';
+    function model_disable (id, cur) {
+        $('#modal-model-alert').modal();
+        document.getElementById('model-alert-url').href = '<?php echo url("admin/model/cdisabled", array("typeid" => $typeid, "modelid" => "")); ?>' + id;
+        document.getElementById('model-alert-body').innerText = cur == 1 ? '确定启用模型吗？' : '禁用之后，该模型的数据将无法访问，确定禁用吗？';
+    }
+    $('#modal-model-alert').on('hide.bs.modal', function() {
+        document.getElementById('model-alert-url').href = '';
+        document.getElementById('model-alert-body').innerText = '';
     })
 </script>
 
