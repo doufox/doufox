61cbc8107cd8e8948dd01562ab6f5970783c8149
doudoudzj <doudoudzj@gmail.com>
2019-11-08 00:56:23 +0800

    更新后台登录页面

--- src/core/views/admin/login.tpl.php
+++ src/core/views/admin/login.tpl.php
@@ -8,27 +8,37 @@
     <meta name="force-rendering" content="webkit" />
     <meta name="renderer" content="webkit" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>网站管理系统 - 管理员登陆</title>
+    <title>网站内容管理系统 - DouFox</title>
     <link rel="icon" type="image/x-icon" href="/favicon.ico" mce_href="/favicon.ico">
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" mce_href="/favicon.ico">
     <meta name="robots" content="none, nofollow, noarchive, nocache">
     <meta name="referrer" content="never" />
     <link type="text/css" href="/static/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <style type="text/css">
-        .panel {
-            max-width: 330px;
-            margin: 16% auto 0 auto;
+        body {
+            background-color: #f3f3f3;
         }
-
+        .header {
+            margin: 16% 0 30px;
+        }
+        .text-muted {
+            font-size: 16px;
+            color: #999;
+        }
+        .panel {
+            max-width: 600px;
+            margin: 0 auto;
+        }
+        .panel-body {
+            padding: 40px;
+        }
+        .row {
+            max-width: 300px;
+            margin: 0 auto;
+        }
         .input-group {
             margin-bottom: 20px;
         }
-
-        .panel-footer {
-            font-size: 12px;
-            text-transform: capitalize;
-        }
-
         #checkcode {
             width: 85px;
             height: 26px;
@@ -37,40 +47,55 @@
 </head>
 
 <body>
-    <div class="panel panel-default text-center">
-        <div class="panel-heading">管理员登陆</div>
-        <div class="panel-body">
-            <form method="POST" action="">
-                <div class="input-group">
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                    <input name="username" type="text" class="form-control" placeholder="账号" required autofocus maxlength="20">
-                </div>
-                <div class="input-group">
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                    <input name="password" type="password" class="form-control" placeholder="密码" maxlength="20" autocomplete="off" required />
-                </div>
-                <div class="input-group">
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                    <input type="text" name="code" class="form-control captcha" placeholder="验证码" maxlength="4" autocomplete="off" />
-                    <span class="input-group-btn">
-                        <button class="btn btn-default" type="button" style="padding: 3px;">
-                            <img id="checkcode" src="" title="看不清楚？换一张" alt="验证码" />
-                        </button>
-                    </span>
-                </div>
-                <button name="submit" type="submit" class="btn btn-primary btn-block">登录</button>
-            </form>
+    <div class="container">
+        <div class="header text-center">
+            <h1>DouFox</h1>
+            <p class="text-muted">网站内容管理系统</p>
         </div>
-        <div class="panel-footer">Copyright &copy; <?php echo date('Y'); ?> Crogram, Inc. All Rights Reserved.</div>
-        <script type="text/javascript">
-            (function() {
-                function checkcode_init() {
-                    document.getElementById("checkcode").src = '<?php echo url("api/access/checkcode", array("width" => 85, "height" => 26)); ?>&' + Math.random();
-                }
-                checkcode_init();
-                document.getElementById("checkcode").onclick = checkcode_init;
-            })();
-        </script>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <span>管理员登陆</span>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <label for="username" class="control-label">账号</label>
+                                <input name="username" type="text" class="form-control" placeholder="输入登录账号" required autofocus maxlength="20" autocomplete="off">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="control-label">密码</label>
+                                <span class="pull-right"><a href="#" title="正在开发中">忘记密码</a></span>
+                                <input name="password" type="password" class="form-control" placeholder="输入登录密码" maxlength="20" autocomplete="off" required />
+                            </div>
+                            <div class="input-group">
+                                <input type="text" name="code" class="form-control captcha" placeholder="右侧验证码" maxlength="4" autocomplete="off" />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button" style="padding: 3px;">
+                                        <img id="checkcode" src="" title="看不清楚？换一张" alt="验证码" />
+                                    </button>
+                                </span>
+                            </div>
+                            <div class="form-group">
+                                <a href="#" title="正在开发中">手机验证码登录</a>
+                            </div>
+                            <button name="submit" type="submit" class="btn btn-primary btn-block">登录</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+        (function() {
+            function checkcode_init() {
+                document.getElementById("checkcode").src = '<?php echo url("api/access/checkcode", array("width" => 85, "height" => 26)); ?>&' + Math.random();
+            }
+            checkcode_init();
+            document.getElementById("checkcode").onclick = checkcode_init;
+        })();
+    </script>
 </body>
 
 </html>
