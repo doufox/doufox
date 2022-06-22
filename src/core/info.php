924b80eb3c5fa9abe11756d9ec6fbd51590661ba
Jackson Dou <jksdou@qq.com>
2022-06-23 02:57:37 +0800

    Update info.php

--- src/core/info.php
+++ src/core/info.php
@@ -10,7 +10,7 @@ if (!defined('IN_CRONLITE')) {
 define('APP_NAME', 'Doufox'); // 系统名称
 define('APP_SITE', 'http://doufox.com'); // 系统官方网站
 define('APP_VERSION', '0.0.3'); // 系统版本
-define('APP_RELEASE', '20220622'); // 系统发布日期
+define('APP_RELEASE', '20220623'); // 系统发布日期
 define('APP_DEBUG', 0); // 系统调试
 header('Content-Type: text/html; charset=utf-8');
 header('X-Powered-By: ' . APP_NAME);
