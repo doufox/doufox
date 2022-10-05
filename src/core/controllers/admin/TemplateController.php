17b6005975f51ecf3a9f7b932db4898b9b969779
Jackson Dou <jksdou@qq.com>
2022-10-05 21:04:18 +0800

    close #7 v0.1.1

--- src/core/controllers/admin/TemplateController.php
+++ src/core/controllers/admin/TemplateController.php
@@ -7,10 +7,12 @@ class TemplateController extends Admin
 {
 
     private $dir;
+    private $allowed_file_suffix;
 
     public function __construct()
     {
         parent::__construct();
+        $this->allowed_file_suffix = array('html', 'css', 'js', 'txt');
     }
 
     public function indexAction()
@@ -96,6 +98,12 @@ class TemplateController extends Admin
         $filename = urldecode($this->get('file'));
         $dir = $this->get('dir') ? urldecode($this->get('dir')) : '/';
         $dir = str_replace(DS . DS, DS, $dir);
+
+        $ext = strtolower(trim(substr(strrchr($filename, '.'), 1, 10)));
+        if (!in_array($ext, $this->allowed_file_suffix)) {
+            $this->show_message($ext . '文件不允许操作！', 2, url('admin/template/item', array('dir' => $dir)));
+        }
+
         $filepath = PATH_TEMPLATE . DS . $template . $dir . $filename;
         $cur_path = DS . DIR_TEMPLATE. DS . $template . $dir . $filename;
         if (!is_file($filepath)) {
@@ -134,7 +142,7 @@ class TemplateController extends Admin
                 $this->show_message('该文件已经存在', 2, url('admin/template/add', array('template' => $template, 'dir' => $dir)));
             }
             $ext = strtolower(trim(substr(strrchr($filename, '.'), 1, 10)));
-            if (!in_array($ext, array('html', 'css', 'js', 'txt'))) {
+            if (!in_array($ext, $this->allowed_file_suffix)) {
                 $this->show_message('文件名后缀不对', 2, url('admin/template/add', array('template' => $template, 'dir' => $dir)));
             }
 
